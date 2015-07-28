97c8886a00003484c0662d6005f9f5315168b655
Jake Song <jksong3@gmail.com>
2015-07-29 02:48:59 +0900

    Initialize repo

---
+++ src/stores/dummyStore.js
@@ -0,0 +1,15 @@
+import alt from 'control';
+import {createStore, bind} from 'alt/utils/decorators';
+import actions from 'actions/dummyActions';
+
+@createStore(alt)
+class DummyStore {
+  name = 'awesome';
+
+  @bind(actions.updateName)
+  updateName(name) {
+    this.name = name;
+  }
+}
+
+export default DummyStore;
