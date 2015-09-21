a0ab7ad4c4e7298093921a530b0edce9bc8d9a7d
Jake Song <jksong3@gmail.com>
2015-09-22 01:23:49 +0900

    clean up login

--- web/static/js/login.jsx
+++ web/static/js/login.jsx
@@ -3,26 +3,19 @@ import { Link } from 'react-router';
 import { Panel, Input, ButtonInput, Row, Col } from 'react-bootstrap';
 
 const panelStyles = { maxWidth: 400, margin: '0 auto' };
+const centerStyle = { width: 200, margin: '20px auto -20px' };
 
 export class Login extends Component {
   render() {
-    var errors = <div></div>;
     return (
       <Panel style={panelStyles}>
-        {errors}
-
-        <div>
-          <form onSubmit={this._onSubmit}>
-            <h2></h2>
-            <Input ref="username" type="text" placeholder="Username" required autofocus />
-            <Input ref="password" type="password" placeholder="Password" required />
-            <Row>
-              <Col xs={4} xsOffset={4}>
-                <ButtonInput type="submit" value="Log in" bsStyle='primary' block />
-              </Col>
-            </Row>
-          </form>
-        </div>
+        <form onSubmit={this._onSubmit}>
+          <Input ref="username" type="text" placeholder="Username" required autofocus />
+          <Input ref="password" type="password" placeholder="Password" required />
+          <div style={centerStyle}>
+            <ButtonInput type="submit" value="Log in" bsStyle='primary' block />
+          </div>
+        </form>
       </Panel>
     );
   }
